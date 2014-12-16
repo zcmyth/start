6e3c0d5724d81e9549ecb757aa3c4f4db5d3f767
Chun Zhang <zhangchun88@gmail.com>
2014-12-16 00:32:51 -0500

    add script for adding event

--- server/script.py
+++ server/script.py
@@ -4,12 +4,29 @@ from flask.ext.script import Manager
 from start import create_app
 from settings import DEV as ENV
 from flask.ext.migrate import Migrate, MigrateCommand
+from start.models import Event
 
 app = create_app(ENV)
 manager = Manager(app)
 
 migrate = Migrate(app, app.db)
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def update_event():
+    event = Event(
+        id=1,
+        description='Ski|Snowboarding Day Trip to Windham Mountain Dec 20',
+        rental=32,
+        lift=40,
+        lesson=24,
+        bus=30,
+        ticket_num=24
+    )
+    app.db.session.merge(event)
+    app.db.session.commit()
+
 
 if __name__ == "__main__":
     manager.run()
