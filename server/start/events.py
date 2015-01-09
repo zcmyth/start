1d263814777d6907a081b49e22a71c91353139d8
Chun Zhang <zhangchun88@gmail.com>
2015-01-09 06:26:13 +0000

    update status page

--- server/start/events.py
+++ server/start/events.py
@@ -15,8 +15,25 @@ def get_event_order_status(id):
     orders = Order.query.filter_by(
         event_id=id,
         status='PAID'
-    ).all()
-    return render_template('status.html', orders=orders)
+    ).order_by(Order.location).order_by(Order.create_time).all()
+    s = {
+        'bus': 0,
+        'lift': 0,
+        'rental': 0,
+        'lesson': 0,
+        'total': 0
+    }
+    for order in orders:
+        if order.bus:
+            s['bus'] += 1
+        if order.lift:
+            s['lift'] += 1
+        if order.rental:
+            s['rental'] += 1
+        if order.lesson:
+            s['lesson'] += 1
+        s['total'] += order.total
+    return render_template('status.html', orders=orders, status=s)
 
 
 @bp.route('/ticketstatusxuning/<int:id>', methods=['GET'])
